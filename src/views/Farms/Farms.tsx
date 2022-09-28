fb89f4cf769cf0ce7cd85f52f7f69ce648643701
Pogrammerr <canozfuttu@hotmail.com>
2022-09-28 22:53:21 +0300

    increased partner pool disappear time

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -57,7 +57,7 @@ const Farms = () => {
   }, [account, dispatch])
 
   const activeFarms = farmsLP.filter((farm) => farm.multiplier !== '0X')
-  const activeInterstellars = interstellars?.filter((interstellar) => interstellar.endBlock + SECONDS_IN_WEEK >= (Date.now() / 1000))
+  const activeInterstellars = interstellars?.filter((interstellar) => interstellar.endBlock + (SECONDS_IN_WEEK * 4) >= (Date.now() / 1000))
   //  const inactiveInterstellars = interstellars?.filter((interstellar) => interstellar.endBlock < (Date.now() / 1000))
 
   const farmsToDisplayWithAPY: FarmWithStakedValue[] = activeFarms.map((farm) => {
